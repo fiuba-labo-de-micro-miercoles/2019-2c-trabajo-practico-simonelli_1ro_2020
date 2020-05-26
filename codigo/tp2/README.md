eab0d27a938f78bf74d5d3203aecce9339b6d8c2
csimonelli <cristian.simonelli@mulesoft.com>
2020-05-25 21:14:45 -0300

    Deleted leeme updated readme fixed typos in main.asm

--- codigo/tp2/README.md
+++ codigo/tp2/README.md
@@ -1,2 +1,3 @@
-# led blink
-Hacer un led titilar cada un segundo
+# switch press
+Hacer que un led prenda cuando se presiona un led.
+Tener en cuenta la resistencia interna de pull up.
--- codigo/tp2/main.asm
+++ codigo/tp2/main.asm
@@ -1,11 +1,10 @@
 ; There is a switch conected between ground and pin PORTB pin 0
 ; When the switch is presed, the led is turned on, when the switch
 ; is released the led is turned off.
-; Pull up version.
 .include "m328pdef.inc"                             ; Valid definitions to 238p
 
 .equ pin_led = 5                                    ; the built in led is the pin 13 (5th pin in B port)
-.equ pin_button = 0x01                              ; pin conected to the input switch 
+.equ pin_button = 0x01                              ; pin connected to the input switch 
 .equ portb_conf = 0x20
 
 .org 0x000                                          ; The next instruction has to be written to add 0x0000
@@ -27,7 +26,7 @@ main:
                 ldi         r21, pin_button         ; Set r21 a mask in order to read only one bit
 read:           in          r20, PINB               ; Read portb
                 and         r20, r21                ; and with the mask
-                breq        led_on                  ; if z flag is seted the port was low, so the switch is pushed
+                breq        led_on                  ; if z flag is setted the port was low, so the switch is pushed
                 cbi         PORTB, pin_led          ; led off
                 jmp         read                    ; goto red
 led_on:         sbi         PORTB, pin_led          ; led on
