2006155328c3d3b4cc5492c28984cf0cff4ca37f
csimonelli <cristian.simonelli@mulesoft.com>
2020-08-15 17:40:52 -0300

    change tabs into spaces

--- codigo/tp5/main.asm
+++ codigo/tp5/main.asm
@@ -19,29 +19,29 @@ main:
                 ldi         r20, LOW(RAMEND)        ; Load r20 with the last ram address lower byte
                 out         spl, r20                ; Load lower byte in sp with r20
 
-				ldi         r20, 0x3f
-				out         PORTB, r20              ; Set the b port pin 
+                ldi         r20, 0x3f
+                out         PORTB, r20              ; Set the b port pin 
                 out         DDRB, r20               ; Set the same port pin as output and input all others
 
-			    ldi         r22, 0xFE
-				out         DDRC, r22
+                ldi         r22, 0xFE
+                out         DDRC, r22
 				
-				ldi         r22, 0xaf
-				sts         ADCSRA, r22             ; start conversion 
-				ldi         r22, 0x60
-				sts         ADMUX, r22
+                ldi         r22, 0xaf
+                sts         ADCSRA, r22             ; start conversion 
+                ldi         r22, 0x60
+                sts         ADMUX, r22
 
-				sei
+                sei
 
-				lds         r22, ADCSRA
-				ori         r22, 0x40
-				sts         ADCSRA, r22             ; start conversion 
+                lds         r22, ADCSRA
+                ori         r22, 0x40
+                sts         ADCSRA, r22             ; start conversion 
                 
 loop:     		
-				jmp         loop     
+                jmp         loop     
 
 
 adc_isr:
-				lds         r16, ADCH
-				out         PORTB, r16
-				reti
+                lds         r16, ADCH
+                out         PORTB, r16
+                reti
