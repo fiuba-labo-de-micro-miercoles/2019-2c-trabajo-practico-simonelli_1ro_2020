b3582220c8f395979e58865425842985228e577a
csimonelli <cristian.simonelli@mulesoft.com>
2020-08-17 12:38:41 -0300

    Added comments, change tabs into spaces

--- codigo/tp6/main.asm
+++ codigo/tp6/main.asm
@@ -15,7 +15,7 @@
 .org 0x0000
             jmp		configuracion
 .org OVF1addr
-			jmp     isr_timovf1
+            jmp     isr_timovf1
 
 .org INT_VECTORS_SIZE
 configuracion:
@@ -24,82 +24,76 @@ configuracion:
                 out     spl,dummyreg
                 ldi     dummyreg,high(RAMEND)
                 out     sph,dummyreg
-                
-                ldi     dummyreg,0xfc	; Port D 2/3
-                out     DDRD,dummyreg
-                ldi     dummyreg,0x03	; Pullups 2/3
-                out     PORTD,dummyreg
-                ldi     dummyreg,0xff	; Port b OUTPUT
-                out     DDRB,dummyreg
+; port config                
+                ldi     dummyreg,0xf3	                  ; Port D 2/3 as input
+                out     DDRD,dummyreg                     ;
+                ldi     dummyreg,0xff	                  ; Port b as output
+                out     DDRB,dummyreg                     ;
 
+; timer 1 config
+                ldi     dummyreg, high(63974)
+                sts     TCNT1H,  dummyreg
+                ldi     dummyreg, low(63974)
+                sts     TCNT1L,  dummyreg
 
-				ldi     dummyreg, high(63974) ; esto no me suena
-				sts     TCNT1H,  dummyreg     ; esta en overflow, para que comparo???
-				ldi     dummyreg, low(63974)
-				sts     TCNT1L,  dummyreg
-
-				ldi     dummyreg, 0
-				sts     tccr1a, dummyreg
+                ldi     dummyreg, 0
+                sts     tccr1a, dummyreg
 
                 sei
 
 main:
-                in      dummyreg, PIND
-				lsl     dummyreg
-				lsl     dummyreg
-				ldi     zl, low(i00)
-				ldi     zh, high(i00)
-				add     zl, dummyreg
-				ldi     dummyreg, 0
-				adc     zh, dummyreg
-				icall
-				sts     tccr1b, prescaler
-				sts     TIMSK1 , timer_on               ; timer 1 overflow
-				jmp     main
+                in      dummyreg, PIND                    ; Read port D
+                andi    dummyreg, 0x0c                    ; this is actually unnessesary, but clear info 
+                                                          ; that coul be added by mistake
+                ldi     zl, low(i00)                      ; Loads Z with the first position of the table
+                ldi     zh, high(i00)                     ; Multiply by 2 is not necesary in this case
+                                                          ;   icall is world adressed
+                add     zl, dummyreg                      ; Ads tableposition + (input selection)
+                ldi     dummyreg, 0                       ;   ( see table definition )
+                adc     zh, dummyreg                      ;
+                icall                                     ; calls whatever z is pointing to
+                sts     tccr1b, prescaler                 ; Sets new prescaler info
+                sts     TIMSK1 , timer_on                 ; Sets new timer status (on or off)
+                jmp     main                              ; loop
 
-
-
-
-
-i00:			ldi     timer_on, 0                       ; time
-				cbi     PORTB, 1
+; Each entry in this table weiths 8 bytes (4 worlds) and represents an user selection
+; After execute any of this entries both prescaler and time_on will be loaded
+i00:            ldi     timer_on, 0                       ; time
+                cbi     PORTB, 1
                 ret
-				nop
-i01:			ldi     prescaler, ( 1<<CS10 | 1<<CS11 )  ; 64 prescaler 
-				ldi     timer_on,  ( 1 << TOIE1 )         ; time
+                nop
+i01:            ldi     prescaler, ( 1<<CS10 | 1<<CS11 )  ; 64 prescaler 
+                ldi     timer_on,  ( 1 << TOIE1 )         ; time
                 ret
-				nop
-i10:			ldi     prescaler, ( 1<<CS12 )            ; 256 prescaler 
-				ldi     timer_on,  ( 1 << TOIE1)          ; time
+                nop
+i10:            ldi     prescaler, ( 1<<CS12 )            ; 256 prescaler 
+                ldi     timer_on,  ( 1 << TOIE1)          ; time
                 ret
-				nop
-i11:			ldi     prescaler, ( 1<<CS10 | 1<<CS12 )  ; time
-				ldi     timer_on,  ( 1 << TOIE1)          ; 1024 prescaler 
+                nop
+i11:            ldi     prescaler, ( 1<<CS10 | 1<<CS12 )  ; time
+                ldi     timer_on,  ( 1 << TOIE1)          ; 1024 prescaler 
                 ret
-                
 
-
-
+; timer 1 overflow isr
 isr_timovf1:
 
-                call    switch_led                  ; Switch led1 state
-				ldi     r24, high(63974)
-				sts     TCNT1H,  r24
-				ldi     r24, low(63974)
-				sts     TCNT1L,  r24
-				reti
+                call    toggle_led                        ; Switch led1 state
+                ldi     r24, high(63974)                  ; Sets value into tcnt1 that makes 
+                sts     TCNT1H,  r24                      ; timer 1 overflows every 1 second aprox
+                ldi     r24, low(63974)                   ;
+                sts     TCNT1L,  r24                      ;
+                reti
 
-
- ; turns on the led 1 if it is turned off and vice versa 
-switch_led:
-                push   r16                          ; Save the r16 value in the stack
-                push   r17                          ; Save the r16 value in the stack
-                in     r16, PORTB                   ; Read port b
-                ldi    r17, 0x02                    ; load 0000 0010
-                eor    r16, r17                     ; switch state of bit 2 in r16
-                out    PORTB, r16                   ; load portb with the bit switched
-                pop    r17                          ; restore r17
-                pop    r16                          ; restore r16
+; turns on the led 1 if it is turned off and vice versa 
+toggle_led:
+                push    r16                               ; Save the r16 value in the stack
+                push    r17                               ; Save the r16 value in the stack
+                in      r16, PORTB                        ; Read port b
+                ldi     r17, 0x02                         ; load 0000 0010
+                eor     r16, r17                          ; switch state of bit 2 in r16
+                out     PORTB, r16                        ; load portb with the bit switched
+                pop     r17                               ; restore r17
+                pop     r16                               ; restore r16
                 ret
 
                      
